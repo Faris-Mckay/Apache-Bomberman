232265988e9e7784c21f2a4e0c7aa47ebf321dd2
Faris-McKay <faris.mckay@hotmail.com>
2016-06-02 01:14:00 +0100

    add module documentation

---
+++ Bomberman Client/src/com/apache/package_info.java
@@ -0,0 +1,27 @@
+/* 
+ * This file is part of Bomberman.
+ *
+ * Copyright (C) Apache-GS, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ *
+ * Further information can be acquired regarding the licensing of this product 
+ * Apache-GS (C). In the project license directory.
+ * Written by Faris McKay <faris.mckay@hotmail.com>, May 2016
+ *
+ */
+package com.apache;
+
+import com.apache.*;
+
+/**
+ *
+ * @author Faris <https://github.com/faris-mckay>
+ */
+public class package_info {
+    
+    /**
+     * Module handles all of the non-network related client processing
+     */
+    
+}
---
+++ Bomberman Server/logic/com/apache/package_info.java
@@ -0,0 +1,25 @@
+/* 
+ * This file is part of Bomberman.
+ *
+ * Copyright (C) Apache-GS, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ *
+ * Further information can be acquired regarding the licensing of this product 
+ * Apache-GS (C). In the project license directory.
+ * Written by Faris McKay <faris.mckay@hotmail.com>, May 2016
+ *
+ */
+package com.apache;
+
+/**
+ *
+ * @author Faris <https://github.com/faris-mckay>
+ */
+public class package_info {
+    
+    /**
+     *  Module handles all of the logical and content aspects of the game
+     */ 
+    
+}
