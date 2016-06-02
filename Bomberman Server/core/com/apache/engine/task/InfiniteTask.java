fc4dffb24979f75c6eb50c0a5ecf27a7da70705d
Faris-McKay <faris.mckay@hotmail.com>
2016-06-02 18:41:50 +0100

    implement server engine

---
+++ Bomberman Server/core/com/apache/engine/task/InfiniteTask.java
@@ -0,0 +1,27 @@
+/* 
+ * This file is property of Apache-GS.
+ *
+ * Copyright (C) Apache-GS, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ *
+ * Further information can be acquired regarding the licensing of this product 
+ * Apache-GS (C). In the project license directory.
+ * Written by Faris McKay <faris.mckay@hotmail.com>, May 2016
+ *
+ */
+package com.apache.engine.task;
+
+import com.apache.engine.Task;
+
+/**
+ *
+ * @author Faris <https://github.com/faris-mckay>
+ */
+public abstract class InfiniteTask extends Task {
+    
+    public InfiniteTask(int cycleRate){
+        super(false, cycleRate);
+    }
+    
+}
